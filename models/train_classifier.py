677b063b7da9b12d3f5691b1ab5d25a5c710a5a7
basimsaed <62621380+basimsaed@users.noreply.github.com>
2023-04-09 12:21:46 +0300

    Add files via upload

---
+++ models/train_classifier.py
@@ -0,0 +1,210 @@
+import sys
+import pandas as pd
+import numpy as np
+import os
+import re
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+from scipy.stats import gmean
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sqlalchemy import create_engine
+
+
+def load_data(database_filepath):
+    """
+    INPUT:
+    database_filepath - 
+    
+    OUTPUT:
+    X - messages (input variable) 
+    y - categories of the messages (output variable)
+    category_names - category name for y
+    """
+    
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('beso_disaster_response_db', engine)
+    
+    X = df['message']
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    return X, y, category_names
+
+
+def tokenize(text):
+    """
+    INPUT:
+    text - raw text
+    
+    OUTPUT:
+    clean_tokens - tokenized messages
+    """
+    
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+        
+    return clean_tokens
+
+
+def build_model(clf = AdaBoostClassifier()):
+    """
+    Build Pipeline function
+    
+    Output:
+        A Scikit ML Pipeline that process text messages and apply a classifier.
+        
+    """
+    
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ]))
+        ])),
+        ('clf', MultiOutputClassifier(clf))
+    ])
+    
+    return pipeline
+
+def multioutput_fscore(y_true,y_pred,beta=1):
+    """
+    MultiOutput Fscore
+    
+    This is a performance metric of my own creation.
+    It is a sort of geometric mean of the fbeta_score, computed on each label.
+    
+    It is compatible with multi-label and multi-class problems.
+    It features some peculiarities (geometric mean, 100% removal...) to exclude
+    trivial solutions and deliberatly under-estimate a stangd fbeta_score average.
+    The aim is avoiding issues when dealing with multi-class/multi-label imbalanced cases.
+    
+    It can be used as scorer for GridSearchCV:
+        scorer = make_scorer(multioutput_fscore,beta=1)
+        
+    Arguments:
+        y_true -> List of labels
+        y_prod -> List of predictions
+        beta -> Beta value to be used to calculate fscore metric
+    
+    Output:
+        f1score -> Calculation geometric mean of fscore
+    """
+    
+    # If provided y predictions is a dataframe then extract the values from that
+    if isinstance(y_pred, pd.DataFrame) == True:
+        y_pred = y_pred.values
+    
+    # If provided y actuals is a dataframe then extract the values from that
+    if isinstance(y_true, pd.DataFrame) == True:
+        y_true = y_true.values
+    
+    f1score_list = []
+    for column in range(0,y_true.shape[1]):
+        score = fbeta_score(y_true[:,column],y_pred[:,column],beta,average='weighted')
+        f1score_list.append(score)
+        
+    f1score = np.asarray(f1score_list)
+    f1score = f1score[f1score<1]
+    
+    # Get the geometric mean of f1score
+    f1score = gmean(f1score)
+    return f1score
+
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Evaluate Model function
+    
+    This function applies a ML pipeline to a test set and prints out the model performance (accuracy and f1score)
+    
+    Arguments:
+        pipeline -> A valid scikit ML Pipeline
+        X_test -> Test features
+        Y_test -> Test labels
+        category_names -> label names (multi-output)
+    """
+    
+    Y_pred = model.predict(X_test)
+    
+    multi_f1 = multioutput_fscore(Y_test,Y_pred, beta = 1)
+    overall_accuracy = (Y_pred == Y_test).mean().mean()
+
+    print('Average overall accuracy {0:.2f}%'.format(overall_accuracy*100))
+    print('F1 score (custom definition) {0:.2f}%'.format(multi_f1*100))
+
+    # Print the whole classification report.
+    Y_pred = pd.DataFrame(Y_pred, columns = Y_test.columns)
+    
+    for column in Y_test.columns:
+        print('Model Performance with Category: {}'.format(column))
+        print(classification_report(Y_test[column],Y_pred[column]))
+        
+    
+
+
+def save_model(model, model_filepath):
+     """
+    INPUT:
+    model - ML model
+    model_filepath - location to save the model
+    
+    OUTPUT:
+    none
+    """
+    
+     with open(model_filepath, 'wb') as b:
+        pickle.dump(model, b)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
